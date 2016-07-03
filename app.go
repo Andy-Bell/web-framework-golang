a6c0edc8bf5acc8323e715c2e6b3f928e0be7d02
Andy <andy.bell.github@gmail.com>
2016-07-03 17:14:32 +0100

    add second route

--- app.go
+++ app.go
@@ -1,13 +1,27 @@
+package main
 import (
   "net/http"
   "fmt"
+  "time"
+  "log"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+  t1 := time.Now()
+  fmt.Fprintf(w, "You are on the about page")
+  t2 := time.Now()
+  log.Printf("[%s] %q v \n", r.Method, r.URL.String(), t2.Sub(t1))
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+  t1 := time.Now()
   fmt.Fprintf(w, "Welcome!")
+  t2 := time.Now()
+  log.Printf("[%s] %q v \n", r.Method, r.URL.String(), t2.Sub(t1))
 }
 
 func main() {
-  http.Handle("/", handler)
+  http.HandleFunc("/", indexHandler)
+  http.HandleFunc("/about", aboutHandler)
   http.ListenAndServe(":8080",nil)
 }
